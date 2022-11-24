b1dbbeab707a5ffd8c7cb9e3dd814f4ee10e9c40
Maximiliano Ortiz <Besysoft@DESKTOP-3ETNGS2>
2022-11-24 11:39:18 -0300

    [Ejercitación-Sección-7]-GenericController

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
@@ -2,9 +2,10 @@ package com.springsimplespasos.universidad.universidadbackend.controlador;
 
 import com.springsimplespasos.universidad.universidadbackend.exception.BadRequestExecption;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.GenericoDAO;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 public class GenericController <E,S extends GenericoDAO<E>> {
 
@@ -25,8 +26,24 @@ public class GenericController <E,S extends GenericoDAO<E>> {
     }
 
     //obtenerPorId(id)
+    @GetMapping("/{id}")
+    public E obtenerPorId(@PathVariable Integer id){
+        Optional<E> entidad = service.findById(id);
+        if(!entidad.isPresent())
+            throw new BadRequestExecption(String.format(nombreEntidad, " con id %d no encontrado"));
+        return entidad.get();
+    }
 
     //borrarEntidadPorId(id)
+    @DeleteMapping("/{id}")
+    public void borrarPorId(@PathVariable Integer id){
+        service.deleteId(id);
+    }
 
     //altaEntidad(Entidad)
+    @PostMapping
+    public E altaEntidad(@RequestBody E entidad){
+        return  service.save(entidad);
+    }
+
 }
